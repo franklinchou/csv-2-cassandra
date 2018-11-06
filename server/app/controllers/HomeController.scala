8c75f3b8fe03200933e2a2908a09ebc3bbf65ae8
Franklin Chou <franklin@peeriq.com>
2018-11-06 09:15:14 -0500

    home controller

--- server/app/controllers/HomeController.scala
+++ server/app/controllers/HomeController.scala
@@ -2,23 +2,20 @@ package controllers
 
 import javax.inject._
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-@Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+@Singleton
+class HomeController @Inject()(config: Configuration,
+                               cc: ControllerComponents) extends AbstractController(cc) {
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(
+      Json.obj(
+        "application" -> "csv-2-cassandra",
+        "environment" -> config.get[String]("app.environment")
+      )
+    )
   }
 }
